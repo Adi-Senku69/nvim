38173c6ae4224daadfeeab5379040636181116b3
FIXED-TERM Vishnuvardhan Aditya (MS/PJ-CoE-AT2) <fixed-term.Aditya.Vishnuvardhan@in.bosch.com>
2025-04-17 17:04:20 +0530

    Batman

---
+++ lua/plugins/treesitter-config.lua
@@ -0,0 +1,18 @@
+return {'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+	  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  config = function()
+	local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
+
+vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep ' })
+local configs = require("nvim-treesitter.configs")
+configs.setup({
+          ensure_installed = { "c", "lua", "vim","javascript", "html", "python", "css" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+  end
+
+} 
