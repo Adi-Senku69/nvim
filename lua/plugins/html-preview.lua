20025f581580a2ba225905cba655c5b01998f9ce
Adi-Senku69 <vaditya06@gmail.com>
2025-04-20 10:49:00 +0530

    Added dynamic port mapping to the html-live preview

--- lua/plugins/html-preview.lua
+++ lua/plugins/html-preview.lua
@@ -1,3 +1,23 @@
+local socket = require("socket")
+
+local function is_port_in_use(port)
+  local tcp = socket.tcp()
+  local success, _ = tcp:bind("127.0.0.1", port)
+  tcp:close()
+  return not success
+end
+
+local function find_available_port(start_port)
+  local port = start_port
+  while is_port_in_use(port) do
+    port = port + 1
+  end
+  return port
+end
+
+local base_port = 5500
+local available_port = find_available_port(base_port)
+
 return {
   "brianhuster/live-preview.nvim",
   dependencies = {
@@ -5,6 +25,8 @@ return {
     "nvim-telescope/telescope.nvim",
   },
   config = function()
-    require("livepreview.config").set()
+    require("livepreview.config").set({
+      port = available_port,
+    })
   end,
 }
