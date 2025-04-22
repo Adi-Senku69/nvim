629be57d2c764870a90797e091bace0fa0456479
Adi-Senku69 <vaditya06@gmail.com>
2025-04-23 00:02:54 +0530

    Added formatting for html files

--- init.lua
+++ init.lua
@@ -43,15 +43,35 @@ vim.lsp.config["pylsp"] = {
     },
   },
 }
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 vim.lsp.config["cssls"] = {
   cmd = { "vscode-css-language-server", "--stdio" },
   filetypes = { "css" },
   root_markers = { ".git", "package.json" },
+  init_options = {
+    provideFormatter = true,
+    embeddedLanguages = {
+      css = true,
+      javascript = true,
+    },
+    configurationSelection = { "html", "css", "javascript" },
+  },
+  capabilities = capabilities,
 }
 vim.lsp.config["html_ls"] = {
   cmd = { "vscode-html-language-server", "--stdio" },
   filetypes = { "html" },
   root_markers = { ".html" },
+  init_options = {
+    provideFormatter = true,
+    embeddedLanguages = {
+      css = true,
+      javascript = true,
+    },
+    configurationSelection = { "html", "css", "javascript" },
+  },
+  capabilities = capabilities,
 }
 vim.lsp.config["ts-ls"] = {
   cmd = { "typescript-language-server", "--stdio" },
