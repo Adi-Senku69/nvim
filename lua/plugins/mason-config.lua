2882881bbf1f05c12ab28727fba411c715b92c04
Aditya <vaditya06@gmail.com>
2025-06-05 00:19:09 +0530

    Added automatic installation of lsp and linters and formatters

--- lua/plugins/mason-config.lua
+++ lua/plugins/mason-config.lua
@@ -1,5 +1,25 @@
-return {     "williamboman/mason.nvim",
-     config = function()
-       require("mason").setup()
-     end,
+return {
+  "williamboman/mason.nvim",
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      -- ensure_installed = { "lua-language-server", "docker-compose-language-service", "dockerfile-language-server", "dotenv-linter", "elixir-ls", "html-lsp", "jedi-language-server", "jq", "prettier", "python-lsp-server", "ruff", "typescript-language-server", "yaml-language-server" },
+      ensure_installed = {
+        "lua_ls",
+        "dockerls",
+        "elixirls",
+        "html",
+        "jedi_language_server",
+        "pylsp",
+        "ts_ls",
+        "yamlls",
+        "docker_compose_language_service"
+      },
+      automatic_installation = true
+    })
+    require("mason-null-ls").setup({
+      ensure_installed = { "prettier", "ruff", "jq", "dotenv-linter" },
+      automatic_installation = true
+    })
+  end,
 }
---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,6 @@
+return {
+  "williamboman/mason.nvim",
+  "jose-elias-alvarez/null-ls.nvim",
+  "jay-babu/mason-null-ls.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+}
